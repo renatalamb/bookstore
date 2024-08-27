7d24786d6f126740a04f1c9ca9cc6dfc43c100da
Renata Lamb <renatalamb95@gmail.com>
2024-08-27 16:14:48 +0100

    feat: wip5

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -154,4 +154,4 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
+DJANGO_ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
