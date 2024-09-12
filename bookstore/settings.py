8459a113268f7810984b9f28e271a4f2a74ba2d3
Renata Lamb <renatalamb95@gmail.com>
2024-09-12 12:43:25 +0100

    fix: corrigi problemas com testes e settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -150,7 +150,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
